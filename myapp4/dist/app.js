499f2d12933de2dc6df1ee195b1ee4bbe2dde612
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-09 21:17:01 +0300

    feat: js files created from ts file with tsc command

--- myapp4/dist/app.js
+++ myapp4/dist/app.js
@@ -4,11 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const utils_1 = require("./utils");
 const app = (0, express_1.default)();
 const port = 3000;
 app.get("/", (request, response) => {
     console.log("Main Page");
     response.send("Hello typescript");
+});
+app.get('/greetings', (request, response) => {
+    const message = (0, utils_1.greet)("World 2");
+    response.send(message);
 });
 app.listen(port, () => {
     console.log(`Server on port: ${port} is up and running...`);
---
+++ myapp4/dist/utils.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.greet = greet;
+/**
+ * Μια διαφορά TS με JS είναι ο τύπος, εδώ string
+ * Επίσης πρέπει να πούμε ότι αυτό που θα επιστρέψει
+ * είναι και αυτό τύπου string (προαιρετικό)
+ */
+function greet(name) {
+    return `Hello ${name}`;
+}
+//# sourceMappingURL=utils.js.map
