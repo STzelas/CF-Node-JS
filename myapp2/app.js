e655a643fc04f237de6503dc0562494124c0d36c
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-09 18:39:37 +0300

    feat: new app.post for /userForm endpoint, comments added for clarifications, simple myform.html created

--- myapp2/app.js
+++ myapp2/app.js
@@ -2,16 +2,32 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+
+// Διαδικασία που μπορούμε να ερθουμε 
+// και να τρέξουμε ενδιάμεσες συναρτήσεις / πχ συναρτήσεις auth
+// Επίσης με το app.use μπορώ να πάρω καποια request(end points)
+// και αναλογα να διοχετεύσω το κώδικα σε άλλες διαδικασίες
+app.use(express.json()); 
+
 app.get("/", (request, response) => {
   response.send("This is the home page")
 })
 
 app.post('/user', (request, response) => {
+  let data = request.body   // request = Αυτό που παίρνω απο το postman
+  let username = request.body.username
+  let email = request.body.email
+
+  console.log(data)
+  response.send(`User data: ${username}, ${email}`)  // response = Αυτό που στέλνω στο postman 
+}) 
+
+app.post("/userForm", (request, response) => {
   let data = request.body
 
   console.log(data)
-  response.send("User page")
-}) 
+  response.send("UserForm page")
+})
 
 
 app.listen(port, () => {
