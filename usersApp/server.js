c397529e6a7f6b8b6ad47b4c95946ce07ac3a8ad
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-10 18:48:23 +0300

    feat: enviromental variable file created with MONGODB connection string inside for security

--- usersApp/server.js
+++ usersApp/server.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose') // library for the database
 const app = require('./app')  // import απο το app.js, το app που εκανα export
 const port = 3000
 
-mongoose.connect("127.0.0.1")   // Εκεί που συνδέεται η βάση είναι. / promise
-        .then(                  // thenables 
+// Το connection string απο το Compass
+// Εκεί που συνδέεται η βάση είναι.
+// retryWrites και w=majority σημαίνουν ότι αν δεν συνδεθεί, θα προσπαθεί ξανα και ξανά μέχρι να συνδεθεί και δεν θα πεταξει ερρορ
+mongoose.connect(process.env.MONGODB_URI)   
+        .then(                  // thenables  / promise
           () => {
             console.log("Connection to mongoDB established")
           },
