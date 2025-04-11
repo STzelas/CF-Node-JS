4192c0552b50d076d826c95788406625bf170d11
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-11 16:10:46 +0300

    feat: find one and create query created in controller

--- usersApp/controllers/user.controller.js
+++ usersApp/controllers/user.controller.js
@@ -1,3 +1,4 @@
+
 const User = require('../models/user.model')
 
 exports.findAll = async(request, response) => {
@@ -12,9 +13,64 @@ exports.findAll = async(request, response) => {
     })
   } catch(err) {
     console.log("Problem in reading users", err)
-    res.json({
+    response.json({
       status: false,
       data: err
     })
   }
 }
+
+exports.findOne = async(request, response) => {
+  console.log("Find user with specific username")
+  let username = request.params.username
+
+  try {
+    const result = await User.findOne({
+      username: username
+    })
+
+    response.json({
+      status: true,
+      data: result
+    })
+  } catch(err) {
+    console.log("Problem in finding user", err)
+    response.json({
+      status: false,
+      data: err
+    })
+  }
+}
+
+exports.create = async(request, response) => {
+  console.log("Create user")
+  
+  let data = request.body
+
+  const newUser = new User({
+    username: data.username,
+    password: data.password,
+    name: data.name,
+    surname: data.surname,
+    email: data.email,
+    address: {
+      area: data.address.area,
+      road: data.address.road
+    }
+  })
+
+  try{
+    const result = await newUser.save();
+    response.json({
+      status: true,
+      data: result
+    })
+  } catch(err) {
+    console.log("Problem in creating user", err)
+    response.json({
+      status: false,
+      data: err
+    })
+  }
+}
+
