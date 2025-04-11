5b053af83b8239d76e0929c73713bf2a8316d519
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-11 16:08:23 +0300

    feat: express filter for json added

--- usersApp/app.js
+++ usersApp/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json()) // Όταν θέλουμε το express να διαβάσει json να στείλει
+
 const user = require('./routes/users.routes')
 
 app.use('/api/users', user)
