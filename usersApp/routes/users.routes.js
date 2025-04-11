406cc13953a1fafdcc26db10be90564a20525194
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-11 20:53:50 +0300

    feat: call to controller for update, delete by username, delete by email and delete all created

--- usersApp/routes/users.routes.js
+++ usersApp/routes/users.routes.js
@@ -7,4 +7,8 @@ const userController = require('../controllers/user.controller')
 router.get('/', userController.findAll)
 router.get('/:username', userController.findOne)
 router.post('/', userController.create)
+router.patch('/:username', userController.update)
+router.delete('/:username', userController.deleteByUsername)
+router.delete('/:username/email/:email', userController.deleteByEmail)
+router.delete('/', userController.deleteAll)
 module.exports = router
