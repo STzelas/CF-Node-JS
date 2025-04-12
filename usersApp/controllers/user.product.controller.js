54feb1b0b2845c44d7bd5164d3ab585b0be47b83
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-12 15:17:22 +0300

    feat: findOne method created for a specific username

--- usersApp/controllers/user.product.controller.js
+++ usersApp/controllers/user.product.controller.js
@@ -17,3 +17,27 @@ exports.findAll = async(request, response) => {
     })
   }
 }
+
+
+exports.findOne = async(request, response) => {
+  console.log("Find products from a specific user")
+
+  const data = request.params
+
+  const username = data.username
+
+  try {
+    const result = await User.findOne({username:username}, {username: 1, products: 1, _id: 0})
+
+    response.status(200).json({
+      status:true,
+      data: result
+    })
+  } catch (err) {
+    console.log("Error in finding specific user", err)
+    response.status(404).json({
+      status:false,
+      data: err
+    })
+  }
+}
