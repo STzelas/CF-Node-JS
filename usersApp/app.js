8839fd3c29aae600de7a16e9d36ca7fb8fbe9484
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-12 15:11:10 +0300

    feat:  endpoint for products created

--- usersApp/app.js
+++ usersApp/app.js
@@ -4,7 +4,9 @@ const app = express()
 app.use(express.json()) // Όταν θέλουμε το express να διαβάσει json να στείλει
 
 const user = require('./routes/users.routes')
+const userProduct = require('./routes/user.products.routes')
 
 app.use('/api/users', user)
+app.use('/api/user-product', userProduct)  // Στα endpoint εδώ βάζουμε -
 
 module.exports = app
