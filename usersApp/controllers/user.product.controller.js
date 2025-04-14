4860683c16a447ec515ab98cb45bdaa4a36cd07d
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-14 14:00:35 +0300

    feat: 'new' field added on update function

--- usersApp/controllers/user.product.controller.js
+++ usersApp/controllers/user.product.controller.js
@@ -87,8 +87,8 @@ exports.update = async(request, response) => {
       {
         $set: {
           "product.$.quantity": product_quantity
-        }
-      }
+        }},
+        {new: true}
     )
     response.status(200).json({
       status: true,
