31669e67a3abae3f251d1d32f3c6a888fa94767f
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-15 21:11:10 +0300

    feat: verifyRoles function imported to routes and added to post call

--- usersApp/routes/users.routes.js
+++ usersApp/routes/users.routes.js
@@ -4,10 +4,11 @@ const router = express.Router()
 
 const userController = require('../controllers/user.controller')
 const verifyToken = require('../middlewares/auth.middleware').verifyToken
+const verifyRoles = require('../middlewares/auth.middleware').verifyRoles
 
 router.get('/', userController.findAll)
 router.get('/:username', userController.findOne)
-router.post('/', verifyToken, userController.create)  // Να γινει verify το auth token στο post route
+router.post('/', verifyToken, verifyRoles("ADMIN"), userController.create)  // Να γινει verify το auth token στο post route
 router.patch('/:username', userController.update)
 router.delete('/:username', userController.deleteByUsername)
 router.delete('/:username/email/:email', userController.deleteByEmail)
