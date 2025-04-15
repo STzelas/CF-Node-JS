a627b9f8bc3f23a3d85a03519918a76f9853da0c
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-15 20:20:12 +0300

    feat: verifytoken function from middleware imported and added to post call

--- usersApp/routes/users.routes.js
+++ usersApp/routes/users.routes.js
@@ -3,10 +3,11 @@ const express = require('express')
 const router = express.Router()
 
 const userController = require('../controllers/user.controller')
+const verifyToken = require('../middlewares/auth.middleware').verifyToken
 
 router.get('/', userController.findAll)
 router.get('/:username', userController.findOne)
-router.post('/', userController.create)
+router.post('/', verifyToken, userController.create)  // Να γινει verify το auth token στο post route
 router.patch('/:username', userController.update)
 router.delete('/:username', userController.deleteByUsername)
 router.delete('/:username/email/:email', userController.deleteByEmail)
