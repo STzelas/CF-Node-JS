cf66d169d77cad4ae0b636168054c94722bdb460
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-19 13:39:03 +0300

    fix

--- usersApp/services/auth.service.js
+++ usersApp/services/auth.service.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-const { OAuth2Client, OAuth2Client } = require('google-auth-library')
+const { OAuth2Client } = require('google-auth-library')
 
 function generateAccessToken(user) {
 
