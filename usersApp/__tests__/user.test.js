2110816e93474d9ce536caa8df7416d0015f1ea4
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-22 17:14:30 +0300

    feat: db closure after each test and test for get all users added

--- usersApp/__tests__/user.test.js
+++ usersApp/__tests__/user.test.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 
 // Connecting to MongoDB before each test
 beforeEach(async()=> {  // διαδικασία του jest 
-  await mongoose.connect(process.env)
+  await mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("Connection to MongoDB established for jest")
   },
@@ -16,3 +16,19 @@ beforeEach(async()=> {  // διαδικασία του jest
     console.log("Failed to connect to MongoDB for jest", err)
   })
 }) 
+
+// Close connection to MongoDB after each test
+afterEach(async()  => {
+  await mongoose.connection.close()
+})
+
+describe("Requests for /api/users", () => {
+  it("GET Returns all users", async () => {  // εκτός απο it μπορούμε και test είναι το ίδιο
+    const res = await request(app)
+      .get('/api/users')
+
+    expect(res.statusCode).toBe(200)
+    expect(res.body.status).toBe(true) // ή toBeTruthy
+    expect(res.body.data.length).toBeGreaterThan(0)
+  }, 50000)                       
+})
