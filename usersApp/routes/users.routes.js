51e723475ed24627b6deb96b01bbde2a51ed8275
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-13 19:59:13 +0300

    feat: new post route for testing perpouses, original commented out

--- usersApp/routes/users.routes.js
+++ usersApp/routes/users.routes.js
@@ -8,7 +8,8 @@ const verifyRoles = require('../middlewares/auth.middleware').verifyRoles
 
 router.get('/', verifyToken, userController.findAll)
 router.get('/:username', verifyToken, userController.findOne)
-router.post('/', verifyToken, verifyRoles("ADMIN"), userController.create)  // Να γινει verify το auth token στο post route
+// router.post('/', verifyToken, verifyRoles("ADMIN"), userController.create)  // Να γινει verify το auth token στο post route
+router.post('/', userController.create) // για test
 router.patch('/:username', verifyToken, verifyRoles("ADMIN"), userController.update)
 router.delete('/:username', verifyToken, verifyRoles("ADMIN"), userController.deleteByUsername)
 router.delete('/:username/email/:email', verifyToken, verifyRoles("ADMIN"), userController.deleteByEmail)
