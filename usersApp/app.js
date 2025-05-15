3353a95927478799e724fff8b5014aa231d4e4ee
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-15 20:49:10 +0300

    fix: origin changed to 4200

--- usersApp/app.js
+++ usersApp/app.js
@@ -14,7 +14,7 @@ const authentication = require('./routes/auth.routes')
 
 
 app.use(cors({
-  origin: ['http://localhost:3000']
+  origin: ['http://localhost:4200']
 }))
 
 app.use('/api/auth', authentication)
